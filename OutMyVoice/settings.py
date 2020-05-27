d9726c8adaecd73c2222c2a07a22b2b76614d557
SulemanMughal <48021243+SulemanMughal@users.noreply.github.com>
2020-05-27 20:53:46 +0500

    Update settings.py

--- OutMyVoice/settings.py
+++ OutMyVoice/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'OutMyVoice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': 
+    {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aggtrends',
+        'USER': 'theo',
+        'PASSWORD': 'testing321',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-}
+ }
 
 
 # Password validation
